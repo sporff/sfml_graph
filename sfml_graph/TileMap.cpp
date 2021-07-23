5efab75296f3514401a4a5b39eb9748af6d69fa7
sporff <sporff@gmail.com>
2021-07-22 21:25:08 -0400

    tilemap size

--- sfml_graph/TileMap.cpp
+++ sfml_graph/TileMap.cpp
@@ -4,3 +4,18 @@ TileMap::TileMap()
 {
 
 }
+
+TileMap::~TileMap()
+{
+
+}
+
+bool TileMap::CreateMap(int w, int h)
+{
+	return false;
+}
+
+void TileMap::DestroyMap()
+{
+
+}
--- sfml_graph/TileMap.h
+++ sfml_graph/TileMap.h
@@ -3,5 +3,13 @@ class TileMap
 {
 public:
 	TileMap();
+	~TileMap();
+
+	bool CreateMap(int w, int h);
+	void DestroyMap();
+
+private:
+	int m_width;
+	int m_height;
 };
 
